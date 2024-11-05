89a7960c33437ce64c0660d8efa465222fbef027
LILAY <contact@lilay.dev>
2024-11-05 14:07:06 +0700

    switch polkit auth & added yazi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,12 @@ alias ls="lsd"
 alias lsa="lsd -a"
 
 zoxide init --cmd cd fish | source
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
