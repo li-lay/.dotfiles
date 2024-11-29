2176ab1afda4407cbcf0ffaf776bf9008e77f04f
LILAY <contact@lilay.dev>
2024-11-29 22:45:21 +0700

    [nvim] added more options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,3 +1,6 @@
+-- Hello World!
+vim.cmd([[autocmd VimEnter * echo "Hello World!"]])
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -14,3 +17,16 @@ vim.opt.mouse = 'a'
 vim.opt.termguicolors = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Highlight yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100 }
+  end,
+})
+
+-- HighlightColors 1
+vim.cmd([[autocmd VimEnter * HighlightColors 1]])
