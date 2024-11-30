b14d6b11b599274a56e67b8ba672647f2afd279f
LILAY <contact@lilay.dev>
2024-11-30 18:26:00 +0700

    [nvim] add indent-blankline

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+-- Indent line
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {},
+	config = function()
+		require("ibl").setup()
+	end,
+}
