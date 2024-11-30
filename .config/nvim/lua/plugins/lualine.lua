59fbcb31fc63e5d2d493b356c9732403126dd455
LILAY <contact@lilay.dev>
2024-11-30 08:51:44 +0700

    [nvim] changes lualine sections

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,24 +1,26 @@
 -- NOTE: Lualine is a statusline
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  opts = {
-    options = {
-      icons_enabled = true,
-      -- theme = "seoul256",
-      theme = "gruvbox",
-      -- theme = "gruvbox-material",
-      -- theme = "codedark",
-      component_separators = "",
-      section_separators = "",
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
-  },
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	opts = {
+		options = {
+			icons_enabled = true,
+			-- theme = "seoul256",
+			theme = "gruvbox",
+			-- theme = "gruvbox-material",
+			-- theme = "codedark",
+			component_separators = "",
+			section_separators = "",
+		},
+		sections = {
+			lualine_a = { "mode" },
+			-- lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_b = { "branch", "diagnostics" },
+			lualine_c = { "filename" },
+			-- lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_x = { "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { { "datetime", style = "%I:%M" } },
+		},
+	},
 }
