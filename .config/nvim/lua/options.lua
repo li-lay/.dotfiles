d6b246bf31a5e2e6e15a82e88681f47e420ecdbf
LILAY <contact@lilay.dev>
2024-12-01 13:06:26 +0700

    [nvim] add more options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,24 +1,35 @@
+---@diagnostic disable: undefined-global
 -- Hello World!
 vim.cmd([[autocmd VimEnter * echo "Hello World!"]])
 
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set wildmenu")
 vim.cmd("set wildmode=longest:full,full")
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
+vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.wrap = true
+vim.opt.showmode = false            -- Don't show the mode, since it's already in the status line
+vim.opt.cursorline = true
+vim.opt.wrap = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
 vim.opt.termguicolors = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.smartindent = true
-vim.opt.scrolloff = 19
+vim.opt.scrolloff = 10
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 
 -- Highlight yank
 vim.api.nvim_create_autocmd("TextYankPost", {
