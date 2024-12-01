f554f214fbf7b0a5e25059881a8ca32114732106
LILAY <contact@lilay.dev>
2024-12-01 09:18:13 +0700

    [nvim] combine plugins into miscellaneous.lua for easy management

---
+++ .config/nvim/lua/plugins/miscellaneous.lua
@@ -0,0 +1,53 @@
+-- This file consist of various plugins
+return {
+	-- SudaWrite
+	{
+		"lambdalisue/suda.vim",
+		cmd = { "SudaRead", "SudaWrite" },
+	},
+	-- Indent line
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+		config = function()
+			require("ibl").setup()
+		end,
+	},
+	-- Colorizer
+	{
+		"NvChad/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = { -- set to setup table
+		},
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+	-- Marks
+	{
+		"chentoast/marks.nvim",
+		opts = {
+			excluded_filetypes = {
+				"qf",
+				"NvimTree",
+				"toggleterm",
+				"TelescopePrompt",
+				"alpha",
+				"netrw",
+				"neo-tree",
+			},
+		},
+	},
+	-- Autopairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+}
--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "NvChad/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
-    },
-  config = function ()
-    require("colorizer").setup()
-  end
-}
