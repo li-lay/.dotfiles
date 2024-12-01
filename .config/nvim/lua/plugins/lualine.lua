50912ab0ae414661de76ac3df3daa43f8ec12a31
LILAY <contact@lilay.dev>
2024-12-01 09:22:12 +0700

    [nvim] disable lualine on nvim-tree

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,6 +11,7 @@ return {
 			-- theme = "codedark",
 			component_separators = "",
 			section_separators = "",
+      disabled_filetypes = { 'NvimTree' }
 		},
 		sections = {
 			lualine_a = { "mode" },
