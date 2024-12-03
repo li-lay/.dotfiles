e9754b6bb9a02b33f1e518b48f3db9e2fc64d9e0
LILAY <contact@lilay.dev>
2024-12-03 09:04:56 +0700

    [nvim] treesitter add more language

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,34 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-    opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "cpp",
-      "html",
-      "css",
-      "javascript",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+		opts = {
+			ensure_installed = {
+				"bash",
+				"cpp",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+				-- add more arguments for adding more treesitter parsers
+			},
+		},
+	},
 }
