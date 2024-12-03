05f9ee8f13037599d40d43f027d70993f39c9c19
LILAY <contact@lilay.dev>
2024-12-03 09:04:33 +0700

    [nvim] add options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -16,6 +16,7 @@ vim.opt.relativenumber = true
 vim.opt.showmode = false -- Don't show the mode, since it's already in the status line
 vim.opt.cursorline = true
 vim.opt.wrap = false
+vim.opt.linebreak = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
 vim.opt.termguicolors = true
