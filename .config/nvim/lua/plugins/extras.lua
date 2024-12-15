f4bef544ed2a68bebb7e71ba2a09d6c2101fe902
LILAY <contact@lilay.dev>
2024-12-15 09:04:56 +0700

    [nvim] change lsp tech stack

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -1,10 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.lsp.none-ls" },
-  { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.clangd" },
-  { import = "lazyvim.plugins.extras.lang.java" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
 }
