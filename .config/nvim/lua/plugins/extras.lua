92c8324288cd96e7391525a5ea812d02c88cfa06
LILAY <contact@lilay.dev>
2024-12-18 16:15:47 +0700

    [nvim] remove lang support

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -1,6 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.dap.core" },
 }
