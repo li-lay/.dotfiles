2ef6d9d407d92fcabb434b2a47c448d9f782e3c9
LILAY <contact@lilay.dev>
2025-01-17 12:01:26 +0700

    [nvim] theme relate changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,12 +5,12 @@ return {
     config = function()
       require("gruvbox").setup({
         contrast = "hard",
-        palette_overrides = {
-          -- dark0 = "#1f1f1f", -- override background
-        },
-        overrides = {
-          -- SignColumn = { bg = "#1f1f1f" },
-        },
+        -- palette_overrides = {
+        --   dark0 = "#1f1f1f", -- override background
+        -- },
+        -- overrides = {
+        --   SignColumn = { bg = "#1f1f1f" },
+        -- },
       })
     end,
   },
