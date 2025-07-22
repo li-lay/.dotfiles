72542bfa5167c4dd1ddc7c22222be9ea4adbc22c
LILAY <contact@lilay.dev>
2025-07-22 10:59:01 +0700

    [neovim] add flash nvim back

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Life Changing --
@@ -6,32 +6,34 @@ vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("i", "kj", "<ESC>")
 
 -- Stuff I want --
-vim.keymap.set("n", "<leader>e", vim.cmd.Oil, { desc = 'File Explorer' })
+vim.keymap.set("n", "<leader>e", vim.cmd.Oil, { desc = "File Explorer" })
 vim.keymap.set("n", "<leader>ww", "<Cmd>w<CR>", { desc = "Save" })
-vim.keymap.set('n', '<Leader>qw', ':wq<CR>', { desc = 'Quit and save' })
-vim.keymap.set('n', '<Leader>qf', ':q!<CR>', { desc = 'Quit without saving' })
-vim.keymap.set('n', '<Leader>qq', ':q<CR>', { desc = 'Quit' })
-vim.keymap.set('n', 'H', '^')
-vim.keymap.set('n', 'L', '$')
+vim.keymap.set("n", "<Leader>qw", ":wq<CR>", { desc = "Quit and save" })
+vim.keymap.set("n", "<Leader>qf", ":q!<CR>", { desc = "Quit without saving" })
+vim.keymap.set("n", "<Leader>qq", ":q<CR>", { desc = "Quit" })
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Tmux --
 local function move_or_tmux(key)
-  local dir_map = {
-    ["<C-h>"] = { win_cmd = "h", tmux_cmd = "L" },
-    ["<C-j>"] = { win_cmd = "j", tmux_cmd = "D" },
-    ["<C-k>"] = { win_cmd = "k", tmux_cmd = "U" },
-    ["<C-l>"] = { win_cmd = "l", tmux_cmd = "R" },
-  }
+	local dir_map = {
+		["<C-h>"] = { win_cmd = "h", tmux_cmd = "L" },
+		["<C-j>"] = { win_cmd = "j", tmux_cmd = "D" },
+		["<C-k>"] = { win_cmd = "k", tmux_cmd = "U" },
+		["<C-l>"] = { win_cmd = "l", tmux_cmd = "R" },
+	}
 
-  return function()
-    local current_win = vim.api.nvim_get_current_win()
-    vim.cmd("wincmd " .. dir_map[key].win_cmd)
+	return function()
+		local current_win = vim.api.nvim_get_current_win()
+		vim.cmd("wincmd " .. dir_map[key].win_cmd)
 
-    if vim.api.nvim_get_current_win() == current_win then
-      -- Didn't move in Neovim, fallback to tmux
-      vim.fn.system({ "tmux", "select-pane", "-" .. dir_map[key].tmux_cmd })
-    end
-  end
+		if vim.api.nvim_get_current_win() == current_win then
+			-- Didn't move in Neovim, fallback to tmux
+			vim.fn.system({ "tmux", "select-pane", "-" .. dir_map[key].tmux_cmd })
+		end
+	end
 end
 vim.keymap.set("n", "<C-h>", move_or_tmux("<C-h>"), { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", move_or_tmux("<C-j>"), { noremap = true, silent = true })
@@ -40,8 +42,8 @@ vim.keymap.set("n", "<C-l>", move_or_tmux("<C-l>"), { noremap = true, silent = t
 
 -- Yank --
 -- OSC Yank
-vim.keymap.set('n', '<leader>yo', '<Plug>OSCYankOperator', { desc = "OSCYank the given text" })
-vim.keymap.set('v', '<leader>yo', '<Plug>OSCYankVisual', { desc = "OSCYank current selection" })
+vim.keymap.set("n", "<leader>yo", "<Plug>OSCYankOperator", { desc = "OSCYank the given text" })
+vim.keymap.set("v", "<leader>yo", "<Plug>OSCYankVisual", { desc = "OSCYank current selection" })
 -- To delete without yanking to any register (the black hole register)
 vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without yanking" })
 -- Yank current file path/name
@@ -49,32 +51,32 @@ vim.keymap.set("n", "<leader>yf", ":let @+=expand('%')<CR>", { desc = "Yank file
 vim.keymap.set("n", "<leader>yF", ":let @+=expand('%:p')<CR>", { desc = "Yank full file path" })
 
 -- Paste --
-vim.keymap.set('x', 'p', '"_dP', { desc = "Paste over selected text without copying" })
+vim.keymap.set("x", "p", '"_dP', { desc = "Paste over selected text without copying" })
 
 -- Buffer navigation --
 vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true, desc = "Delete current buffer" })
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", { noremap = true, silent = true, desc = "Delete current buffer" })
 
 -- Move lines up/down --
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 local function move_or_tmux(key)
-  local dir_map = {
-    ["<C-h>"] = { win_cmd = "h", tmux_cmd = "L" },
-    ["<C-j>"] = { win_cmd = "j", tmux_cmd = "D" },
-    ["<C-k>"] = { win_cmd = "k", tmux_cmd = "U" },
-    ["<C-l>"] = { win_cmd = "l", tmux_cmd = "R" },
-  }
+	local dir_map = {
+		["<C-h>"] = { win_cmd = "h", tmux_cmd = "L" },
+		["<C-j>"] = { win_cmd = "j", tmux_cmd = "D" },
+		["<C-k>"] = { win_cmd = "k", tmux_cmd = "U" },
+		["<C-l>"] = { win_cmd = "l", tmux_cmd = "R" },
+	}
 
-  return function()
-    local current_win = vim.api.nvim_get_current_win()
-    vim.cmd("wincmd " .. dir_map[key].win_cmd)
+	return function()
+		local current_win = vim.api.nvim_get_current_win()
+		vim.cmd("wincmd " .. dir_map[key].win_cmd)
 
-    if vim.api.nvim_get_current_win() == current_win then
-      -- Didn't move in Neovim, fallback to tmux
-      vim.fn.system({ "tmux", "select-pane", "-" .. dir_map[key].tmux_cmd })
-    end
-  end
+		if vim.api.nvim_get_current_win() == current_win then
+			-- Didn't move in Neovim, fallback to tmux
+			vim.fn.system({ "tmux", "select-pane", "-" .. dir_map[key].tmux_cmd })
+		end
+	end
 end
 
 -- Bind the keys
