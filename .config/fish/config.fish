e881b0aa159293ac929766ace56df7d7daddf11f
LILAY <contact@lilay.dev>
2025-08-06 13:31:36 +0700

    [fish] remove some unused functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,22 +18,6 @@ end
 # Vim
 fish_vi_key_bindings
 
-function backup --argument filename
-    cp $filename $filename.bak
-end
-
-# Copy DIR1 DIR2
-function copy
-    set count (count $argv | tr -d \n)
-    if test "$count" = 2; and test -d "$argv[1]"
-        set from (echo $argv[1] | trim-right /)
-        set to (echo $argv[2])
-        command cp -r $from $to
-    else
-        command cp $argv
-    end
-end
-
 ## Useful aliases
 # Replace ls with eza
 alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
