7a83c1dd889b8c09844013dfbc265a15edbc91cf
LILAY <contact@lilay.dev>
2025-10-06 18:05:18 +0700

    [neovim] optimize startup time

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,31 +1,33 @@
 return {
-	"github/copilot.vim", -- :Copilot setup
-	"yetone/avante.nvim",
-	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-	-- ⚠️ must add this setting! ! !
-	build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-		or "make",
-	event = "VeryLazy",
-	version = false, -- Never set this value to "*"! Never!
-	---@module 'avante'
-	---@type avante.Config
-	opts = {
-		instructions_file = "avante.md",
-		provider = "openrouter",
-		providers = {
-			openrouter = {
-				__inherited_from = "openai",
-				endpoint = "https://openrouter.ai/api/v1",
-				api_key_name = "OPENROUTER_API_KEY",
-				model = "z-ai/glm-4.6",
+	{ "github/copilot.vim", event = "InsertEnter" }, -- :Copilot setup
+	{
+		"yetone/avante.nvim",
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		-- ⚠️ must add this setting! ! !
+		build = vim.fn.has("win32") ~= 0
+				and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+			or "make",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		---@module 'avante'
+		---@type avante.Config
+		opts = {
+			instructions_file = "avante.md",
+			provider = "openrouter",
+			providers = {
+				openrouter = {
+					__inherited_from = "openai",
+					endpoint = "https://openrouter.ai/api/v1",
+					api_key_name = "OPENROUTER_API_KEY",
+					model = "z-ai/glm-4.6",
+				},
 			},
 		},
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		--- The below dependencies are optional,
-		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-		"echasnovski/mini.icons",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		},
 	},
 }
