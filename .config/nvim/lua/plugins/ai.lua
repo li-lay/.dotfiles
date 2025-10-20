f72942cd7e82eeeea652714aa064f13accd6c0e9
LILAY <contact@lilay.dev>
2025-10-20 19:16:38 +0700

    [nvim] disable avante.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -22,34 +22,34 @@ return {
 			})
 		end,
 	},
-	{
-		"yetone/avante.nvim",
-		-- If you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		-- ⚠️ must add this setting! ! !
-		build = vim.fn.has("win32") ~= 0
-				and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-			or "make",
-		event = "InsertEnter",
-		version = false, -- Never set this value to "*"! Never!
-		---@module 'avante'
-		---@type avante.Config
-		opts = {
-			instructions_file = "avante.md",
-			provider = "openrouter",
-			providers = {
-				openrouter = {
-					__inherited_from = "openai",
-					endpoint = "https://openrouter.ai/api/v1",
-					api_key_name = "OPENROUTER_API_KEY",
-					model = "z-ai/glm-4.6",
-				},
-			},
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-		},
-	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	-- If you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- 	-- ⚠️ must add this setting! ! !
+	-- 	build = vim.fn.has("win32") ~= 0
+	-- 			and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+	-- 		or "make",
+	-- 	event = "InsertEnter",
+	-- 	version = false, -- Never set this value to "*"! Never!
+	-- 	---@module 'avante'
+	-- 	---@type avante.Config
+	-- 	opts = {
+	-- 		instructions_file = "avante.md",
+	-- 		provider = "openrouter",
+	-- 		providers = {
+	-- 			openrouter = {
+	-- 				__inherited_from = "openai",
+	-- 				endpoint = "https://openrouter.ai/api/v1",
+	-- 				api_key_name = "OPENROUTER_API_KEY",
+	-- 				model = "z-ai/glm-4.6",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		--- The below dependencies are optional,
+	-- 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+	-- 	},
+	-- },
 }
