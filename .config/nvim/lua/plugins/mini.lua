fcf0287cb90f4a5cd5e8ee0dcb258a31f57e7602
LILAY <contact@lilay.dev>
2026-01-27 08:02:47 +0700

    [nvim] add mini Git

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("mini.ai").setup({})
+		require("mini.git").setup({})
 		require("mini.comment").setup({})
 		require("mini.icons").setup({})
 		require("mini.indentscope").setup({ symbol = "╎" })
